a287a5cbce1f975286e2a4d2664b97f58d8a9561
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-18 15:41:02 -0700

    components initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import "./dummy-store";
+import Header from "./Header/Header";
 export default class App extends React.Component {
+  state = {
+    folders: [],
+    notes: [],
+  };
   render() {
     return (
-      <div className="App">
-        <p>Here is the App Component</p>
-      </div>
+      <main className="App">
+        <Header />
+        <BrowserRouter>
+          <Route exact path="/" component={Main} />
+          <Route path="/folder/:folderId" component={Main} />
+          <Route path="/note/:noteId" component={NoteDetail} />
+          <Route path="/add-folder" component={AddFolder} />
+          <Route path="/add-note" component={AddNote} />
+        </BrowserRouter>
+      </main>
     );
   }
 }
