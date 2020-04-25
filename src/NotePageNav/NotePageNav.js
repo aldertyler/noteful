3a55e589e9982119a92f8912d82b273fc344686e
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-25 10:23:10 -0700

    updated NotePageNav

--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,27 +1,38 @@
 import React from "react";
 import Button from "../Button/Button";
+import ApiContext from "../ApiContext";
+import { findNote, findFolder } from "../notes-helpers";
 import "./NotePageNav.css";
 
-export default function NotePageNav(props) {
-  return (
-    <div className="NotePageNav">
-      <Button
-        tag="button"
-        role="link"
-        onClick={() => props.history.goBack()}
-        className="NotePageNav__back-button"
-      >
-        Back
-      </Button>
-      {props.folder && (
-        <h3 className="NotePageNav__folder-name">{props.folder.name}</h3>
-      )}
-    </div>
-  );
-}
+export default class NotePageNav extends React.Component {
+  static defaultProps = {
+    history: {
+      goBack: () => {},
+    },
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
 
-NotePageNav.defaultProps = {
-  history: {
-    goBack: () => {},
-  },
-};
+  render() {
+    const { notes, folders } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+    return (
+      <div className="NotePageNav">
+        <Button
+          tag="button"
+          role="link"
+          onClick={() => this.props.history.goBack()}
+          className="NotePageNav__back-button"
+        >
+          <br />
+          Back
+        </Button>
+        {folder && <h3 className="NotePageNav__folder-name">{folder.name}</h3>}
+      </div>
+    );
+  }
+}
